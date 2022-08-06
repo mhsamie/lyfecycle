ea3c9f89d29f256b08020e0903e4b4b7cbccdff1
mhsamie <sfatima7720@gmail.com>
2022-08-06 12:24:51 +0430

    comment section added

--- src/container/CommentSection.js
+++ src/container/CommentSection.js
@@ -1,6 +1,11 @@
 import "./comments.css";
+import commentTag from "../images/comment.png";
+import smile from "../images/smile.png";
+import glow from "../images/glow.png";
+import quote from "../images/quote-down.png";
 import Slider from "react-slick";
 import PrimeryBtn from "../UI/PrimeryBtn";
+import profile from "../images/profile.png";
 const CommentSection = () => {
   const settings = {
     dots: true,
@@ -10,13 +15,96 @@ const CommentSection = () => {
     slidesToScroll: 2,
   };
   return (
-    <section>
-      <div className="cpmment-navbar"></div>
-      <Slider {...settings}>
-        <div>1</div>
-        <div>2</div>
-      </Slider>
-      <PrimeryBtn />
+    <section className="comments">
+      <div className="comment-navbar">
+        <img src={commentTag} alt="comment tag" className="commentTag" />
+        <h2>
+          What our Trainees <span>Say About Us</span>
+        </h2>
+        <img src={smile} alt="smile" className="smile-img" />
+        <img src={glow} alt="glow" className="glow-img" />
+        <img src={quote} alt="qoute" className="quote-img" />
+      </div>
+      <div className="slider-container">
+        <Slider {...settings}>
+          <div>
+            <div className="comment">
+              <p>
+                “Hyped, inspired, and empowered. Those are the feels. My music
+                is intense and I don’t mind if you want to ride or dance to it
+              </p>
+              <div className="personal-info">
+                <p>
+                  Mohammed Salah<span>Football Player, Egypt</span>
+                </p>
+                <img src={profile} alt="profile" />
+              </div>
+            </div>
+          </div>
+          <div>
+            <div className="comment">
+              <p>
+                “Hyped, inspired, and empowered. Those are the feels. My music
+                is intense and I don’t mind if you want to ride or dance to it
+              </p>
+              <div className="personal-info">
+                <p>
+                  Mohammed Salah<span>Football Player, Egypt</span>
+                </p>
+                <img src={profile} alt="profile" />
+              </div>
+            </div>
+          </div>
+          <div>
+            <div className="comment">
+              <p>
+                “Hyped, inspired, and empowered. Those are the feels. My music
+                is intense and I don’t mind if you want to ride or dance to it
+              </p>
+              <div className="personal-info">
+                <p>
+                  Mohammed Salah<span>Football Player, Egypt</span>
+                </p>
+                <img src={profile} alt="profile" />
+              </div>
+            </div>
+          </div>
+          <div>
+            <div className="comment">
+              <p>
+                “Hyped, inspired, and empowered. Those are the feels. My music
+                is intense and I don’t mind if you want to ride or dance to it
+              </p>
+              <div className="personal-info">
+                <p>
+                  Mohammed Salah<span>Football Player, Egypt</span>
+                </p>
+                <img src={profile} alt="profile" />
+              </div>
+            </div>
+          </div>
+          <div>
+            <div className="comment">
+              <p>
+                “Hyped, inspired, and empowered. Those are the feels. My music
+                is intense and I don’t mind if you want to ride or dance to it
+              </p>
+              <div className="personal-info">
+                <p>
+                  Mohammed Salah
+                  <span>Football Player, Egypt</span>
+                </p>
+
+                <img src={profile} alt="profile" />
+              </div>
+            </div>
+          </div>
+        </Slider>
+      </div>
+      <div className="btn-container">
+        {" "}
+        <PrimeryBtn />
+      </div>
     </section>
   );
 };
